13f658b9f05b7fccad39ed3e387686b4be15b9d8
Peter <15019313+JiahongChen@users.noreply.github>
2021-05-07 11:14:49 -0700

    fix bug

--- model/net.py
+++ model/net.py
@@ -7,20 +7,6 @@ from torch.autograd import Variable, Function
 import torch.nn.functional as F
 import math
 
-# class GradReverse(Function):
-#     def __init__(self, lambd):
-#         self.lambd = lambd
-
-#     def forward(self, x):
-#         return x.view_as(x)
-
-#     def backward(self, grad_output):
-#         return (grad_output * self.lambd)
-
-
-# def grad_reverse(x, lambd=-1.0):
-#     return GradReverse(lambd)(x)
-
 class GradReverse(Function):
 
     @staticmethod
